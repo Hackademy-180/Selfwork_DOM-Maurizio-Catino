5acae743b57c5b89ed1a7c7ed98511a7b66d3eb4
Maurizio Catino <mauriziocatino96@gmail.com>
2025-11-24 15:35:05 +0100

    Correzioni esercizio 1

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 let div = document.querySelector(`#padre`);
 let paragraf = document.createElement(`p`)
 paragraf.innerHTML = "Sono un tag p figlio di div row"
-section.appendChild(paragraf)
+div.appendChild(paragraf)
 
 console.dir(paragraf)
 
@@ -14,48 +14,48 @@ console.dir(paragraf)
 // il secondo bottone dovra’ cambiare i colore dei paragrafi, 
 // il terzo bottone dovra’ renderli in grassetto e farli tornare come prima.
 
-let div1 = document.querySelectorAll(`#div-padre`)
-let btn1 = document.querySelector("#uno")
-let checked = false
+// let div1 = document.querySelectorAll(`#div-padre`)
+// let btn1 = document.querySelector("#uno")
+// let checked = false
 
-btn1.addEventListener(`click`, ()=>{
-    div1.forEach((div) => {
-        if(checked == false){
-            div.classList.add("d-none");
-            checked = true;
-        }else{
-            div.classList.remove("d-none");
-        checked = false;}
+// btn1.addEventListener(`click`, ()=>{
+//     div1.forEach((div) => {
+//         if(checked == false){
+//             div.classList.add("d-none");
+//             checked = true;
+//         }else{
+//             div.classList.remove("d-none");
+//         checked = false;}
         
-    })
-});
+//     })
+// });
 
-let btn2 = document.querySelector("#due")
+// let btn2 = document.querySelector("#due")
 
-btn2.addEventListener(`click`, ()=>{
-    div1.forEach((div) => {
-        if(checked == false){
-            div.classList.add("text-white", "bg-danger");
-            checked = true;
-        }else{
-            div.classList.remove("text-white", "bg-danger");
-        checked = false;}
+// btn2.addEventListener(`click`, ()=>{
+//     div1.forEach((div) => {
+//         if(checked == false){
+//             div.classList.add("text-white", "bg-danger");
+//             checked = true;
+//         }else{
+//             div.classList.remove("text-white", "bg-danger");
+//         checked = false;}
         
-    })
-})
+//     })
+// })
 
-let btn3 = document.querySelector("#tre")
+// let btn3 = document.querySelector("#tre")
 
-btn3.addEventListener(`click`, ()=>{
-    div1.forEach((div) => {
-        if(checked == false){
-            div.classList.add("fw-bold");
-            checked = true;
-        }else{
-            div.classList.remove("fw-bold");
-        checked = false;}
+// btn3.addEventListener(`click`, ()=>{
+//     div1.forEach((div) => {
+//         if(checked == false){
+//             div.classList.add("fw-bold");
+//             checked = true;
+//         }else{
+//             div.classList.remove("fw-bold");
+//         checked = false;}
         
-    })
-})
+//     })
+// })
 
 
